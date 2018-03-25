984fdcf4df8df8d6ab782bd9f9cd4a367338a7c5
LeAcid <jayge.ekenstam90@gmail.com>
2018-03-24 22:29:31 -0600

    update test and some acessor and setter methods

--- src/AcidState.php
+++ src/AcidState.php
@@ -4,15 +4,63 @@ namespace AcidState;
 
 class AcidState
 {
-    private $state;
+    private $transitions = [];
 
-    public function __construct()
+    /**
+     * Create a new AcidState
+     * 
+     * @return AcidState\AcidState self
+     */
+    static public function create()
     {
-        $this->state = "TEST";
+        return new self;
     }
 
-    public function currentState()
+    /**
+     * Add possible state transitions
+     * cannot be redefined on the fly.
+     * 
+     * @return AcidState\AcidState $this
+     */
+    public function setTransitions(...$transitions)
+    {
+        if (count($this->transitions) == 0) {
+
+            $this->setInitialState($transitions[0]);
+
+            $this->transitions = $transitions;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Return a list of transitions
+     * 
+     * @return array $this->transistions
+     */
+    public function getTransitions()
+    {
+        return $this->transitions;
+    }
+
+    /**
+     * Return current state
+     * 
+     * @return string $this->state
+     */
+    public function getCurrentState()
     {
         return $this->state;
+    }
+
+    /**
+     * Set initial state
+     * 
+     * @return void
+     */
+    private function setInitialState($state)
+    {
+        $this->state = $state;
     }
 }
--- tests/AcidStateTest.php
+++ tests/AcidStateTest.php
@@ -3,11 +3,41 @@
 namespace Tests;
 
 use Tests\BaseCase;
+use AcidState\AcidState;
 
 class AcidStateTest extends BaseCase
 {
     public function testTests()
     {
         $this->assertTrue(true);
+    }
+
+    /**
+     * Test dynamic register state progression
+     * 
+     * @return void
+     */
+    public function testDynamicRegisterStateProgression()
+    {
+        $acidState = AcidState::create()
+            ->setTransitions('armed', 'price_requested', 'price_agreed', 'sold');
+
+        $this->assertEquals(
+            ['armed', 'price_requested', 'price_agreed', 'sold'],
+            $acidState->getTransitions()
+        );
+    }
+
+    /**
+     * Test can receive current state
+     * 
+     * @return void
+     */
+    public function testCanReceiveCurrentState()
+    {
+        $acidState = AcidState::create()
+            ->setTransitions('armed', 'price_requested', 'price_agreed', 'sold');
+
+        $this->assertEquals('armed', $acidState->getCurrentState());
     }
 }
