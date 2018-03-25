75ce825a2cc1ddc6391bd14331cd36f8005d61a8
LeAcid <jayge.ekenstam90@gmail.com>
2018-03-25 13:45:38 -0600

    add example of acid state in index.php

--- index.php
+++ index.php
@@ -4,13 +4,32 @@ require __DIR__ . '/vendor/autoload.php';
 
 use AcidState\AcidState;
 
+$price = null;
+$confirmed = 'N';
+
 $acidState = AcidState::create()
-    ->setTransitions('one', 'two', 'three', 'four');
+    ->setTransitions('armed', 'confirmed', 'sold');
 
-$acidState->nextState();
+while($acidState->getCurrentState() != 'sold') {
+    switch ($acidState->getNextState()) {
+        case 'confirmed':
+            if ($confirmed == 'Y') {
+                $acidState->nextState();
+            } else {
+                echo "You must confirm your intent sell product\n";
+                $confirmed = readline("Enter 'Y' to confirm: ");
+            }
+        break;
+        case 'sold':
+            if ($price) {
+                $acidState->nextState();
+            } else {
+                echo "You must set price to sell vehicle\n";
+                $price = readline("Enter a price: $");
+            }
+        break;
+    }
+}
 
-$acidState->nextState();
-
-$acidState->rollback();
-
+var_dump($price);
 var_dump($acidState->json());
